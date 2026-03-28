dff908d49e9d9232af5969907158f60d50b1bd5e
kPlavsic <kplavsic4@gmail.com>
2026-03-28 09:41:35 +0100

    dodat model Lokacija

---
+++ mikroservisnaApp/Models/Lokacija.cs
@@ -0,0 +1,15 @@
+﻿namespace mikroservisnaApp.Models
+{
+    public class Lokacija
+    {
+        public int Id { get; set; }
+
+        public string Naziv { get; set; }
+
+        public string Adresa { get; set; }
+
+        public int Kapacitet { get; set; }
+
+        public List<Dogadjaj> Dogadjaji { get; set; }
+    }
+}
