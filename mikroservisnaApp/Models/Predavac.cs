890c7f54172ab47d0d7232d13fd25e8314b169a5
kPlavsic <kplavsic4@gmail.com>
2026-03-28 09:39:25 +0100

    dodat model Predavac

---
+++ mikroservisnaApp/Models/Predavac.cs
@@ -0,0 +1,13 @@
+﻿namespace mikroservisnaApp.Models
+{
+    public class Predavac
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Titula { get; set; }
+        public string OblastStrucnosti { get; set; }
+
+        public List<Angazovanje> Angazovanja { get; set; }
+    }
+}
