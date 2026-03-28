6b912d0963d8c32280383a95d00260f6eefcc85a
kPlavsic <kplavsic4@gmail.com>
2026-03-28 09:36:33 +0100

    dodat model Dogadjaj

---
+++ mikroservisnaApp/Models/Dogadjaj.cs
@@ -0,0 +1,21 @@
+﻿namespace mikroservisnaApp.Models
+{
+    public class Dogadjaj
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Agenda { get; set; }
+        public DateTime DatumIVreme { get; set; }
+        public int Trajanje { get; set; }
+        public decimal CenaKotizacije { get; set; }
+
+        public int LokacijaId { get; set; }
+        public Lokacija Lokacija { get; set; }
+
+        public int TipDogadjaja { get; set; }
+        public TipDogadjaja TipDogadjaja { get; set; }
+
+        public List<Angazovanje> Angazovanja { get; set; }
+
+    }
+}
